c9c029db96ad48447b1684c4c8e3615edf856a0d
Daxi Parmar <daxiparmar327@gmail.com>
2026-07-20 21:06:17 +0530

    WIP: MongoDB connection debugging

--- db.py
+++ db.py
@@ -1,10 +1,18 @@
 import os
+import certifi
+
+
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URI"))
+client = MongoClient(
+    os.getenv("MONGO_URI"),
+    tls=True,
+    tlsCAFile=certifi.where()
+)
 
 db = client["expense_tracker"]
 
