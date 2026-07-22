c386217d85ea34141e9e05b619e8be0c6d396604
Daxi Parmar <daxiparmar327@gmail.com>
2026-07-22 19:01:16 +0530

    test mongodb database connection

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
-from db import expenses_collection
+from db import client, expenses_collection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/test-db")
+def test_db():
+    try:
+        # Ping MongoDB server
+        client.admin.command("ping")
+
+        # Optional: Insert a test document
+        expenses_collection.insert_one({
+            "message": "MongoDB Connected Successfully!"
+        })
+
+        return " MongoDB Connected Successfully!"
+
+    except Exception as e:
+        return f" Connection Failed: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- db.py
+++ db.py
@@ -1,11 +1,9 @@
 import os
 import certifi
-
 from dotenv import load_dotenv
 from pymongo import MongoClient
-
-
 load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(
     os.getenv("MONGO_URI"),
@@ -14,5 +12,4 @@ client = MongoClient(
 )
 
 db = client["expense_tracker"]
-
 expenses_collection = db["expenses"]
