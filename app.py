b1dbaf20ded57f7fb362a836f003951470a71032
Daxi Parmar <daxiparmar327@gmail.com>
2026-07-24 21:35:03 +0530

    Implement expense submission route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from db import client, expenses_collection
 
 app = Flask(__name__)
@@ -17,15 +17,24 @@ def test_db():
         expenses_collection.insert_one({
             "message": "MongoDB Connected Successfully!"
         })
-
         return " MongoDB Connected Successfully!"
 
     except Exception as e:
         return f" Connection Failed: {e}"
 
-    @app.route("/add", methods=["POST"])
-    def add_expense():
-    pass
+@app.route("/add", methods=["POST"])
+def add_expense():
+    date = request.form["date"]
+    price = float(request.form["price"])   
+    item = request.form["item"]   
+
+    expense = {
+        "date": date,
+        "price": price,
+        "item": item
+    }
+    expenses_collection.insert_one(expense)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
